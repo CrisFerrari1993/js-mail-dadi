030c4e3a662e821086a00f03f387eee6a57dd613
Cristopher Ferrari <ferraricristopher93@gmail.com>
2023-11-22 16:25:15 +0100

    fine e consegna

--- Esercizio Dadi/js/script.js
+++ Esercizio Dadi/js/script.js
@@ -4,16 +4,30 @@ Gioco dei dadi:
     1)  Generare un numero random da 1 a 6, sia per il giocatore sia per il computer
     2)  Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
+alert('Benvenuto! Oggi tenterai la tua sorte sfidando il Pc a dadi')
 
-let tuaScelta = Math.floor((Math.random() * 6) + 1);
-console.log(tuaScelta);
-let pcScelta = Math.floor((Math.random() * 6) + 1);
-console.log(pcScelta);
+let btnConfirm = document.getElementById('premi');
 
-if (pcScelta < tuaScelta) {
-    console.log('Hai vinto');
-} else if (tuaScelta === pcScelta) {
-    console.log('Pareggio');
-} else {
-    console.log('Hai perso');
-}
+btnConfirm.addEventListener('click',
+    function(){
+        let tuaScelta = Math.floor((Math.random() * 6) + 1);
+        console.log(tuaScelta);
+        let pcScelta = Math.floor((Math.random() * 6) + 1);
+        console.log(pcScelta);
+
+        if (pcScelta < tuaScelta) {
+            console.log('Hai vinto');
+            alert('Hai vinto!!!')
+            document.getElementById('resoult').innerHTML = 'Hai vinto perchè il dado ha dato: ' + tuaScelta + ' e il quello del pc: ' + pcScelta;
+        } else if (tuaScelta === pcScelta) {
+            alert('Pare3ggio!!!')
+            document.getElementById('resoult').innerHTML = 'Pareggio perchè il dado ha dato: ' + tuaScelta + ' e il quello del pc: ' + pcScelta;
+            console.log('Hai perso');
+        } else {console.log('Hai vinto');
+            alert('Hai Perso!!!')
+            document.getElementById('resoult').innerHTML = 'Hai perso perchè il dado ha dato: ' + tuaScelta + ' e il quello del pc: ' + pcScelta;
+            console.log('Hai perso');
+        }
+    }
+);
+
